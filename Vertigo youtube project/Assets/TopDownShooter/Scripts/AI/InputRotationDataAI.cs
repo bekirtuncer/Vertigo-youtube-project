fa28db5db16afe19181fa1df9cd4ce9de5bd0017
Bekir Tuncer <bekirtuncer97@gmail.com>
2021-11-01 15:50:04 +0300

    -new ai system

---
+++ Vertigo youtube project/Assets/TopDownShooter/Scripts/AI/InputRotationDataAI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter.AI
+{
+    [CreateAssetMenu(menuName = "TopDownShooter/Input/AI/Rotation Input Data")]
+    public class InputRotationDataAI : InputDataAI
+    {
+        public override void ProcessInput()
+        {
+            base.ProcessInput();
+        }
+    }
+}
