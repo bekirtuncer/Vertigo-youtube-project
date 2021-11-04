709fec8a1bb938adbbb1317d54ed1efb170e0419
Bekir Tuncer <bekirtuncer97@gmail.com>
2021-11-05 01:35:58 +0300

    -created basic scene manager -created join random room

--- Vertigo youtube project/Assets/TopDownShooter/Scripts/Inventory/ScriptableShootManager.cs
+++ Vertigo youtube project/Assets/TopDownShooter/Scripts/Inventory/ScriptableShootManager.cs
@@ -5,7 +5,7 @@ using TopDownShooter.Stat;
 
 namespace TopDownShooter.Inventory 
 {
-    [CreateAssetMenu(menuName ="TopDownShooter/Inventory/ScriptableShootMagar")]
+    [CreateAssetMenu(menuName ="TopDownShooter/Inventory/ScriptableShootManager")]
     public class ScriptableShootManager : AbstractScriptableManager<ScriptableShootManager>
     {
         public override void Initialize()
--- Vertigo youtube project/Assets/TopDownShooter/Scripts/Manager/AbstractScriptableManager.cs
+++ Vertigo youtube project/Assets/TopDownShooter/Scripts/Manager/AbstractScriptableManager.cs
@@ -1,22 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 namespace TopDownShooter 
 { 
     public class AbstractScriptableManager<T> : AbstractScriptableManagerBase where T : AbstractScriptableManager<T>
     {
+        protected CompositeDisposable _compositeDisposable = null; 
         public static T Instance;
 
         public override void Initialize()
         {
             base.Initialize();
             Instance = this as T;
+            _compositeDisposable = new CompositeDisposable();
         }
 
         public override void Destroy()
         {
             base.Destroy();
+            _compositeDisposable.Dispose();
         }
     }
 }
